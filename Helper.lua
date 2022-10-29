a16818566f629f54a6b744cd4cd47af83b46b4b1
kojakk <price.ag@gmail.com>
2022-10-28 22:56:19 -0500

    Update Helper.lua

--- Helper.lua
+++ Helper.lua
@@ -212,15 +212,30 @@ end
 
 function MaxDps:CheckTalents()
 	self.PlayerTalents = {};
+    
+    -- last selected configID or fall back to default spec config
+    local configID = C_ClassTalents.GetActiveConfigID();
+    
+    local configInfo = C_Traits.GetConfigInfo(configID);
+    local treeID = configInfo.treeIDs[1];
 
-	for talentRow = 1, 7 do
-		for talentCol = 1, 3 do
-			local _, _, _, sel, _, id = GetTalentInfo(talentRow, talentCol, 1);
-			if sel then
-				self.PlayerTalents[id] = 1;
-			end
-		end
-	end
+    local nodes = C_Traits.GetTreeNodes(treeID);
+
+    for _, nodeID in ipairs(nodes) do
+        local nodeInfo = C_Traits.GetNodeInfo(configID, nodeID)
+        if nodeInfo.currentRank and nodeInfo.currentRank > 0 then
+            local entryID = nodeInfo.activeEntry and nodeInfo.activeEntry.entryID and nodeInfo.activeEntry.entryID;
+            local entryInfo = entryID and C_Traits.GetEntryInfo(configID, entryID);
+            local definitionInfo = entryInfo and entryInfo.definitionID and C_Traits.GetDefinitionInfo(entryInfo.definitionID);
+
+            if definitionInfo ~= nil then
+				self.PlayerTalents[definitionInfo.spellID] = 1;
+				--local talentName = TalentUtil.GetTalentName(definitionInfo.overrideName, definitionInfo.spellID);
+                --print(string.format("%s %d", talentName, definitionInfo.spellID));
+            end
+        end
+    end
+
 end
 
 MaxDps.isMelee = false;
