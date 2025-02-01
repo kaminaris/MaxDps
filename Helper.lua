be396a84db1c5214adccdc0a61874609786b2200
tbkizle <tbkizle@gmail.com>
2025-02-01 16:17:19 -0500

    [Helper] Fix CheckSpellUsable for Classic

--- Helper.lua
+++ Helper.lua
@@ -1939,7 +1939,9 @@ function MaxDps:CheckSpellUsable(spell,spellstring)
         end
     end
     if not MaxDps:IsRetailWow() then
-        if not IsSpellKnownOrOverridesKnown(spell) then return false end
+        if MaxDps:IsCataWow() then
+            if not IsSpellKnownOrOverridesKnown(spell) then return false end
+        end
         if not C_Spell.IsSpellUsable(spell) then return false end
         local costs = C_Spell.GetSpellPowerCost(spell)
         if type(costs) ~= 'table' and spellstring then return true end
